094f3acb10109d96c8816a88021c0a479f069df0
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-09 20:39:38 +0900

    [Add] club 스키마 생성

---
+++ back/models/club.js
@@ -0,0 +1,74 @@
+import Sequelize from "sequelize";
+
+module.exports = class Club extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      club_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(45),
+        allowNull: false,
+      },
+      picture: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+      },
+      intro: {
+        type: Sequelize.STRING(300),
+        allowNull: false,
+      },
+      day: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      views: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      num: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      process: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("Now"),
+      },
+      is_deleted: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+        defaultValue: "0",
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  }
+};
