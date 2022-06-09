98f39cef7745f4ee059c22926e838efa2d9d3b3a
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-09 20:42:14 +0900

    [Add] club 테이블 DB에 생성

--- back/models/index.js
+++ back/models/index.js
@@ -3,6 +3,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require("../config/config.json")[env];
 // 모델
 import User from "./user";
+import Club from "./club";
 
 const db = {};
 const sequelize = new Sequelize(
@@ -14,6 +15,7 @@ const sequelize = new Sequelize(
 // db 객체에 모델 정보들 넣음
 db.sequelize = sequelize;
 db.User = User;
+db.Club = Club;
 
 //모델 - 테이블 연결
 User.init(sequelize);
