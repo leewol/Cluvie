6b0e6b7bfe95e7b1db4f0e7423d0023b3dacc92e
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-10 10:21:43 +0900

    [Chore] User branch pull

--- back/index.js
+++ back/index.js
@@ -1,16 +1,8 @@
 import cors from "cors";
 import express from "express";
-import * as dotenv from "dotenv";
-// const dotenv = require("dotenv").config();
-import passport from "passport";
-import passportConfig from "./passport/index";
-import session from "express-session";
-import cookieParser from "cookie-parser";
 import db from "./models/index";
-// import userAuthRouter from "./src/routers/userRouter";
-import clubRouter from "./src/routes/club/router";
-dotenv.config();
-passportConfig();
+import userAuthRouter from "./src/routes/register/router";
+
 const app = express();
 
 const PORT = 5001;
@@ -25,25 +17,15 @@ db.sequelize
   });
 
 app.use(cors({ origin: true, credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  session({
-    saveUninitialized: false,
-    resave: false,
-    secret: process.env.DB_PASSWORD,
-  })
-);
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(express.urlencoded({ extended: false })); // ? 알아내자
+app.use(express.json()); //->req.body 가 잘 보내짐
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
-// app.use(userAuthRouter);
-app.use(clubRouter);
+app.use(userAuthRouter);
+app.user(clubRouter);
 app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다. http://localhost:${PORT}`);
 });
