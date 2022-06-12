6ea471d5e4bbe79f8143ff22b256764733e5eb84
leewol <lsee5265@gmail.com>
2022-06-12 14:36:00 +0900

    [Chore] App.tsx, index.tsx 수정 (Router)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Main from "./pages/Main";
+import { Login, Register, RegisterForm } from "./pages/User";
 
 function App() {
-  return <div className="App">초기 세팅</div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='RegisterByEmail' element={<RegisterForm />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Login, Register, RegisterForm } from "./pages/User";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="RegisterByEmail" element={<RegisterForm />} />
-      </Routes>
-    </BrowserRouter>
-    
+    <App />
   </React.StrictMode>
 );
 
