2174bf25000b35ebba5818043ee09cea8363ee3d
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-13 19:35:52 +0900

    [Feat] 모임 삭제 API 작성

--- back/src/routes/club/router.js
+++ back/src/routes/club/router.js
@@ -73,12 +73,13 @@ club_router.put("/clubs/:id", async (req, res) => {
     });
 });
 
+// 없는 모임을 삭제할 경우, 에러 처리
 club_router.delete("/clubs/:id", async (req, res) => {
   Clubs.destroy({
     where: { id: req.params.id },
   })
     .then((result) => {
-      res.status(200).json({ success: true, result });
+      res.status(200).json({ success: true });
     })
     .catch((err) => {
       res.status(404).json({ success: false, err });
