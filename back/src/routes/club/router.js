0b2cd1fa424a63bd52a659da9cf72009e1d60b31
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-13 18:30:16 +0900

    [Fix] 모임 id별로 조회 해결

--- back/src/routes/club/router.js
+++ back/src/routes/club/router.js
@@ -38,7 +38,7 @@ club_router.get("/clubs", async (req, res) => {
 });
 
 club_router.get("/clubs/:id", async (req, res) => {
-  await Clubs.findOne(req.params.id)
+  await Clubs.findOne({ where: { id: req.params.id } })
     .then((result) => {
       res.status(200).json({ success: true, result });
     })
