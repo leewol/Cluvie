211563cf755d6c6d6e1676902e83c6cd1e99bea8
singahyeon12 <singahyeon12@naver.com>
2022-06-14 20:43:24 +0900

    [Feat] 클럽생성페이지 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "@/pages/Main";
 import { Login, Register, RegisterForm } from "@/pages/User";
 import ClubDetail from "@/pages/ClubDetail/ClubDetail";
+import ClubCreate from "@/pages/ClubCreate/ClubCreate";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='register' element={<Register />} />
           <Route path='registerByEmail' element={<RegisterForm />} />
           <Route path='clubDetail' element={<ClubDetail />} />
+          <Route path='clubCreate' element={<ClubCreate />} />
         </Routes>
       </BrowserRouter>
     </div>
