71b856459eb6169b45658cc14eb0f4d63c16e136
leewol <lsee5265@gmail.com>
2022-06-14 16:35:47 +0900

    [Style] 클럽 카드 (1개) 컴포넌트

---
+++ front/src/pages/ClubList/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ClubCard from "@/components/ClubCard";
+
+function ClubList() {
+  return (
+    <div>
+      <ClubCard />
+    </div>
+  );
+}
+
+export default ClubList;
