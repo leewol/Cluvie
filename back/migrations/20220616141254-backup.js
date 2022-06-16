88ac59ec85a5b8084381bd995d9269b2427a20cd
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-16 23:27:44 +0900

    [Fix] clubs 테이블 필드 수정 (migration)

--- back/migrations/20220616141254-backup.js
+++ back/migrations/20220616141254-backup.js
@@ -2,6 +2,18 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
+    queryInterface.addColumn("clubs", "manager_id", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
+    queryInterface.addColumn("clubs", "duration_of_progress", {
+      type: Sequelize.INTEGER(10),
+      allowNull: false,
+    });
+    queryInterface.addColumn("clubs", "club_state", {
+      type: Sequelize.STRING(10),
+      allowNull: false,
+    });
     /**
      * Add altering commands here.
      *
