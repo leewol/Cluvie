0e6867606d602ecd25df6336ee50732e0a013342
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-17 16:49:17 +0900

    [Chore] 필요없는 코드 주석처리

--- back/migrations/20220616141254-backup.js
+++ back/migrations/20220616141254-backup.js
@@ -2,30 +2,30 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("clubs", "online");
-    await queryInterface.removeColumn("clubs", "offline");
-    await queryInterface.removeColumn("clubs", "weekday");
-    await queryInterface.removeColumn("clubs", "weekend");
-    await queryInterface.addColumn("clubs", "online", {
-      type: Sequelize.TINYINT,
-      allowNull: true,
-      defaultValue: 0,
-    });
-    await queryInterface.addColumn("clubs", "offline", {
-      type: Sequelize.TINYINT,
-      allowNull: true,
-      defaultValue: 0,
-    });
-    await queryInterface.addColumn("clubs", "weekday", {
-      type: Sequelize.TINYINT,
-      allowNull: true,
-      defaultValue: 0,
-    });
-    await queryInterface.addColumn("clubs", "weekend", {
-      type: Sequelize.TINYINT,
-      allowNull: true,
-      defaultValue: 0,
-    });
+    // await queryInterface.removeColumn("clubs", "online");
+    // await queryInterface.removeColumn("clubs", "offline");
+    // await queryInterface.removeColumn("clubs", "weekday");
+    // await queryInterface.removeColumn("clubs", "weekend");
+    // await queryInterface.addColumn("clubs", "online", {
+    //   type: Sequelize.TINYINT,
+    //   allowNull: true,
+    //   defaultValue: 0,
+    // });
+    // await queryInterface.addColumn("clubs", "offline", {
+    //   type: Sequelize.TINYINT,
+    //   allowNull: true,
+    //   defaultValue: 0,
+    // });
+    // await queryInterface.addColumn("clubs", "weekday", {
+    //   type: Sequelize.TINYINT,
+    //   allowNull: true,
+    //   defaultValue: 0,
+    // });
+    // await queryInterface.addColumn("clubs", "weekend", {
+    //   type: Sequelize.TINYINT,
+    //   allowNull: true,
+    //   defaultValue: 0,
+    // });
   },
 
   async down(queryInterface, Sequelize) {
