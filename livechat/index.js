d6c7d975bd3b44ea99e0fcc6adea4606e64cf086
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-23 13:04:53 +0900

    [Feat] 채팅 서버 create

--- livechat/index.js
+++ livechat/index.js
@@ -1,39 +1,20 @@
-import cors from "cors";
-import express, { application } from "express";
-import db from "./models/index";
-import clubRouter from "./src/routes/club/router";
-import userRouter from "./src/routes/user/router";
-import likeRouter from "./src/routes/like/router";
-import applicantRouter from "./src/routes/applicant/router";
-import dotenv from "dotenv";
-dotenv.config();
+const app = require("express");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-const app = express();
-
-const PORT = 5001;
-
-db.sequelize
-  .sync()
-  .then(() => {
-    console.log("데이터 베이스 연결 성공");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.use(cors({ origin: true, credentials: true }));
-// app.use(express.urlencoded({ extended: false })); // ? 알아내자
-app.use(express.json()); //->req.body 가 잘 보내짐
-
-app.get("/", (req, res) => {
-  res.send("Hello Express");
+app.use("/", (req, res) => {
+  res.sendFile(__dirname) + "/index.html";
 });
 
-app.use("/users", userRouter);
-app.use("/likes", likeRouter);
-app.use("/clubs", clubRouter);
-app.use("/applications", applicantRouter);
-
-app.listen(PORT, () => {
-  console.log(`정상적으로 서버를 시작하였습니다. http://localhost:${PORT}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+http.listen(3000, () => {
+  console.log("Connected at 3000");
 });
