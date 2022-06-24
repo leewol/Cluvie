b8059a0f40c51c1597a383dd5264b1d574a97e2b
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-24 15:31:52 +0900

    [Feat] 기본 설정

---
+++ livechat/server.js
@@ -0,0 +1,10 @@
+// var express = require("express");
+// var app = express();
+// var redisAdapter = require("socket.io-redis");
+// var server = require("http").Server(app);
+// var io = require("socket.io")(server);
+// var redis = require("redis");
+// const path = require("path");
+
+// io.eio.pingTimeout = 60*60*1000;
+// io.eio.pingInterval = 5*1000;
