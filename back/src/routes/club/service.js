58af635aa170767da2546e5957d665cc25066bc7
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-27 16:10:37 +0900

    [Chore] 4개 -> 6개 변경

--- back/src/routes/club/service.js
+++ back/src/routes/club/service.js
@@ -56,7 +56,7 @@ class clubService {
     const clubList = await Clubs.findAll({});
     console.log("확인:", club_id);
 
-    let sql = `SELECT * FROM clubs WHERE id <= ${club_id}  ORDER BY id DESC LIMIT  4`;
+    let sql = `SELECT * FROM clubs WHERE id <= ${club_id}  ORDER BY id DESC LIMIT  6`;
     const scrollClubList = await db.sequelize.query(sql, {
       type: db.sequelize.QueryTypes.SELECT,
     });
