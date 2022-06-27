80825d37b27493703304e9e310164981f35c0a77
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-27 21:42:51 +0900

    [Feat] socket 통신 작성

--- livechat/index.js
+++ livechat/index.js
@@ -4,8 +4,9 @@ import { createServer } from "http";
 const fs = require("fs");
 const app = express();
 const server = createServer(app);
-const socketio = new Server(server);
+const io = new Server(server);
 import dotenv from "dotenv";
+import { disconnect } from "process";
 dotenv.config();
 
 const PORT = 5001;
@@ -23,6 +24,33 @@ app.get("/", (req, res) => {
       res.write(data);
       res.end();
     }
+  });
+});
+
+io.sockets.on("connection", (socket) => {
+  socket.on("new User", (name) => {
+    console.log(name, "님이 접속하였습니다.");
+    socket.name = name;
+    io.sockets.emit("update", {
+      type: "connect",
+      name: "SERVER",
+      message: name + "님이 접속하였습니다.",
+    });
+  });
+
+  socket.on("message", (data) => {
+    data.name = socket.name;
+    console.log(data);
+    socket.broadcast.emit("update", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.name + "님이 나가셨습니다.");
+    socket.broadcast.emit("update", {
+      type: disconnect,
+      name: "SERVER",
+      message: socket.name + "님이 나가셨습니다.",
+    });
   });
 });
 
