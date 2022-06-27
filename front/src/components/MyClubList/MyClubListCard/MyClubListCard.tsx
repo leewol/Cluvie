64f8738ef5fa5beae045c2c96c51bfa32c29b0a3
singahyeon12 <singahyeon12@naver.com>
2022-06-27 12:32:22 +0900

    [Chore] 주석제거

--- front/src/components/MyClubList/MyClubListCard/MyClubListCard.tsx
+++ front/src/components/MyClubList/MyClubListCard/MyClubListCard.tsx
@@ -228,14 +228,6 @@ function MyClubListCard({
             </AccordionSummary>
             <AccordionDetails>
               {applicantsList.length === 0 && <div>클럽원이 없습니다.</div>}
-              {/* {!(applicantsList.length === 0) &&
-                applicantsList.map((applicants) => {
-                  if (applicants["status"] === 1) {
-                    return (
-                      <div key={applicants["id"]}>{applicants["nickname"]}</div>
-                    );
-                  }
-                })} */}
               {!(applicantsList.length === 0) &&
                 applicantsList
                   .filter((applicants) => applicants["status"] === 1)
