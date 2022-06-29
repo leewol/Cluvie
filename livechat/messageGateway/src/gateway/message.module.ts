acfc0de8957168cd5c9a159a803d0d7b4374abce
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-29 21:25:18 +0900

    [Chore] livechat 삭제

--- livechat/messageGateway/src/gateway/message.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MessageGateway } from './message.gateway';
-import { MessageService } from './message.service';
-
-@Module({
-  providers: [MessageGateway, MessageService],
-})
-export class MessageModule {}
