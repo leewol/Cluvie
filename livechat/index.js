599ce12cc520da9fd2b73df0bba5c4c0f9c7b960
[4기-2반] 장소영 <soy5360@naver.com>
2022-06-29 22:29:52 +0900

    [Feat] 기본 채팅 구현

--- livechat/index.js
+++ livechat/index.js
@@ -1,16 +1,56 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
+const fs = require("fs");
 const app = express();
 const server = createServer(app);
-const socketio = new Server(server);
+const io = new Server(server);
 import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = 5001;
 
+app.use("/css", express.static("./static/css"));
+app.use("/js", express.static("./static/js"));
+
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  fs.readFile("./static/js/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send("에러");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+io.sockets.on("connection", (socket) => {
+  socket.on("newUser", (name) => {
+    console.log(name + "님이 접속하였습니다.");
+    socket.name = name;
+    io.sockets.emit("update", {
+      type: "connect",
+      name: "SERVER",
+      message: name + "님이 접속하였습니다.",
+    });
+  });
+
+  socket.on("message", (data) => {
+    data.name = socket.name;
+    console.log(data);
+    socket.broadcast.emit("update", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.name + "님이 나가셨습니다.");
+    socket.broadcast.emit("update", {
+      type: "disconnect",
+      name: "SERVER",
+      message: socket.name + "님이 나가셨습니다.",
+    });
+  });
 });
 
 server.listen(PORT, () => {
