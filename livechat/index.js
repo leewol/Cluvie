c18b7bad1067be3db27931ae65e8f3b52bc6d868
[4기-2반] 장소영 <soy5360@naver.com>
2022-07-01 15:45:06 +0900

    [Chore] 채팅 마무리

--- livechat/index.js
+++ livechat/index.js
@@ -25,62 +25,77 @@ app.use("/css", express.static("./static/css"));
 app.use("/js", express.static("./static/js"));
 app.use(livechatRouter);
 
-app.get("/", (req, res) => {
-  fs.readFile("./static/js/index.html", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.send("에러");
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    }
-  });
+app.get("/:club_id", (req, res) => {
+  // fs.readFile("./static/js/index.html", (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //     res.send("에러");
+  //   } else {
+  //     res.writeHead(200, { "Content-Type": "text/html" });
+  //     res.write(data);
+  //     res.end();
+  //   }
+  // });
+  console.log("채팅방 이름 : " + req.params.club_id);
+  res.render("index", { room: req.params.room });
 });
 
-let numUsers = 0;
+// let numUsers = 0;
+
+// io.sockets.on("connection", (socket) => {
+//   let addedUser = false;
+//   socket.on("newMessage", (data) => {
+//     console.log(`${socket.username} : ${data}`);
+//     socket.broadcast.emit("newMessage", {
+//       username: socket.username,
+//       message: data,
+//     });
+//   });
+
+//   socket.on("addUser", (username) => {
+//     if (addedUser) return;
+
+//     socket.username = username;
+//     ++numUsers;
+//     console.log("connected : " + socket.id + "num : " + numUsers);
+//     addedUser = true;
+//     socket.emit("login", {
+//       numUsers: numUsers,
+//     });
+//     socket.broadcast.emit("userJoined", {
+//       username: socket.username,
+//       numUsers: numUsers,
+//     });
+//   });
+
+//   socket.on("typing", () => {
+//     socket.broadcast.emit("typing", {
+//       usernames: socket.username,
+//     });
+//   });
+
+//   socket.on("disconnect", () => {
+//     if (addedUser) {
+//       --numUsers;
+//       console.log("disconnected" + socket.id + "num : " + numUsers);
+//       socket.broadcast.emit("userLeft", {
+//         username: socket.username,
+//         numUsers: numUsers,
+//       });
+//     }
+//   });
+// });
 
 io.sockets.on("connection", (socket) => {
-  let addedUser = false;
-  socket.on("newMessage", (data) => {
-    console.log(`${socket.username} : ${data}`);
-    socket.broadcast.emit("newMessage", {
-      username: socket.username,
-      message: data,
-    });
+  var roomName = null;
+  socket.on("join", (data) => {
+    roomName = data;
+    socket.join(data);
   });
 
-  socket.on("addUser", (username) => {
-    if (addedUser) return;
-
-    socket.username = username;
-    ++numUsers;
-    console.log("connected : " + socket.id + "num : " + numUsers);
-    addedUser = true;
-    socket.emit("login", {
-      numUsers: numUsers,
-    });
-    socket.broadcast.emit("userJoined", {
-      username: socket.username,
-      numUsers: numUsers,
-    });
-  });
-
-  socket.on("typing", () => {
-    socket.broadcast.emit("typing", {
-      usernames: socket.username,
-    });
-  });
-
-  socket.on("disconnect", () => {
-    if (addedUser) {
-      --numUsers;
-      console.log("disconnected" + socket.id + "num : " + numUsers);
-      socket.broadcast.emit("userLeft", {
-        username: socket.username,
-        numUsers: numUsers,
-      });
-    }
+  socket.on("message", (data) => {
+    socket.in(roomName).emit("message", data);
+    console.log(data);
   });
 });
 
