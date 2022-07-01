3f3e72d99d767884c7caea727b09c8a6b97a868f
leewol <lsee5265@gmail.com>
2022-07-01 16:25:37 +0900

    [Style] h2 폰트 크기

--- front/src/components/Main/MainClubList/MainClubListStyle.tsx
+++ front/src/components/Main/MainClubList/MainClubListStyle.tsx
@@ -3,7 +3,7 @@ import styled from "@emotion/styled/macro";
 const CardListBox = styled.div`
   width: 1250px;
   h2 {
-    font-size: 32px;
+    font-size: 30px;
     margin: 80px 0 20px 0;
   }
 `;
